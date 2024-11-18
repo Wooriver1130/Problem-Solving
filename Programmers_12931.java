eb406cd65ee8028d3206be0b991aef9f829e7fe6
=aphel <dldnrkdqkqh@gmail.com>
2024-11-18 19:42:09 +0900

    프로그래머스LV1 자릿수 더하기
    자릿수 더하기

---
+++ Programmers_12931.java
@@ -0,0 +1,27 @@
+package Problem_Solving;
+
+
+public class Programmers_12931 {
+public static void main(String[] args) {
+
+
+//	    String str = String.valueOf(n);
+//		int answer = 0 ;
+//		StringBuffer sb = new StringBuffer();
+//		for (int i = 0; i < str.length(); i++) {
+//			sb.append(str.charAt(i)).append(" ");
+//		}
+//		StringTokenizer st = new StringTokenizer(sb.toString());
+//		
+//		while (st.hasMoreTokens()) {
+//			answer += Integer.parseInt(st.nextToken()) ;
+//		}
+//	        return answer;
+//	        
+//	    }
+//	}
+	
+}
+}
+
+
