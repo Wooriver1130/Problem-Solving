4d675e55be5bc5e0e035fae719db48e836eb5867
=aphel <dldnrkdqkqh@gmail.com>
2024-11-21 16:32:53 +0900

    프로그래머스/Lv1/문자열 내 p와 y의 개수

--- src/main/java/com/ict/problem/solving/Programmers_12916.java
+++ src/main/java/com/ict/problem/solving/Programmers_12916.java
@@ -1,5 +1,26 @@
 package com.ict.problem.solving;
 
 public class Programmers_12916 {
+public static void main(String[] args) {
 
+    // class Solution {
+    //     boolean solution(String str) {
+    //         int countP = 0 ;
+    //         int countY = 0;
+    //         boolean result  =  true;
+    //          for (int i = 0; i < str.length(); i++) {
+    //             if (String.valueOf(str.charAt(i)).equalsIgnoreCase("p")) {
+    //                 countP++;
+    //              }else if(String.valueOf(str.charAt(i)).equalsIgnoreCase("y")){
+    //                countY++;
+    //              }
+    //          }
+    //         if (countP != countY) {
+    //             result = false;
+    //         }
+    //         return result ;
+        
+    // }
+    // }
+}
 }
