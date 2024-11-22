220e3dbcff10c6dd173f829124df2667b70156db
=aphel <dldnrkdqkqh@gmail.com>
2024-11-22 15:19:45 +0900

    프로그래머스/LV1/자연수 뒤집어 배열로 만들기

---
+++ src/main/java/com/ict/problem/solving/Programmers_12932.java
@@ -0,0 +1,17 @@
+package com.ict.problem.solving;
+
+public class Programmers_12932 {
+public static void main(String[] args) {
+    // class Solution {
+    //     public int[] solution(long number) {
+    //         int[] arr = {};
+    //         String str = String.valueOf(number);
+    //          arr = new int[str.length()];
+    //         for(int i = 0; i < arr.length; i++ ) {
+    //            arr[i] = Integer.parseInt(String.valueOf(str.charAt((arr.length-1) - i)));
+    //         }
+    //         return arr;
+    //     }
+    }
+}
+}
