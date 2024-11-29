eb97bb319917e97859a136568b82ff789c299249
=aphel <dldnrkdqkqh@gmail.com>
2024-11-29 17:16:17 +0900

    프로그래머스/lv1/정수 내림차순으로 배치하기

---
+++ src/main/java/com/ict/problem/solving/Programmers_12933.java
@@ -0,0 +1,30 @@
+package com.ict.problem.solving;
+
+import java.util.Arrays;
+
+public class Programmers_12933 {
+    public static void main(String[] args) {
+        // String str = String.valueOf(n);
+        // int[] arr = new int[str.length()];
+        // int tmp = 0;
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     arr[i] = Integer.parseInt(str.charAt(i) + "");
+        // }
+        // for (int i = 0; i < arr.length - 1; i++) {
+        //     for (int j = i + 1; j < arr.length; j++) {
+        //         if (arr[i] < arr[j]) {
+        //             tmp = arr[i];
+        //             arr[i] = arr[j];
+        //             arr[j] = tmp;
+        //         }
+        //     }
+        // }
+        // StringBuffer sb = new StringBuffer();
+        // for (int k = 0; k < arr.length; k++) {
+        //     sb.append(arr[k]);
+        // }
+        // long result = Long.parseLong(sb.toString());
+        // return result;
+    }
+}
