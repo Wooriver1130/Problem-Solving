ae690b6ebb8358618a8f84232949d5b994fc6d7a
=aphel <dldnrkdqkqh@gmail.com>
2024-11-30 20:22:27 +0900

    프로그래머스/Lv1/정주 제곱근 판별

---
+++ src/main/java/com/ict/problem/Programmers_12934.java
@@ -0,0 +1,14 @@
+package com.ict.problem;
+
+public class Programmers_12934 {
+    // public long function(long n){
+    //     long result = -1 ;
+    //     for (long i = 1;  i <= n; i++) {
+    //         if ((i* i) == n) {
+    //             result = (i+1) * (i+i);
+    //             break;
+    //          }
+    //     }
+    //     return result;
+    // }
+}
