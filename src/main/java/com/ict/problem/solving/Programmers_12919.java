3f3db847470bc44382962f0a4a5b3f8fef20cbf7
=aphel <dldnrkdqkqh@gmail.com>
2024-12-04 14:22:52 +0900

    프로그래머스/Lv1/서울에서 김서방 찾기

---
+++ src/main/java/com/ict/problem/solving/Programmers_12919.java
@@ -0,0 +1,17 @@
+package com.ict.problem.solving;
+
+public class Programmers_12919 {
+    // class Solution {
+    //     public String solution(String[] seoul) {
+    //         // String형 배열 seoul의 element중 Kim의 위치 x를 찾아 
+    //         // "김서방은 x에 있다"는 String을 반환하는 함수를 완성하자.
+    //         String result = "";
+    //         for(int i = 0; i < seoul.length; i++) {
+    //             if(seoul[i].equals("Kim")) {
+    //                 result = "김서방은 " + i + "에 있다";
+    //             }
+    //         }
+    //         return result;
+    //     }
+    // }
+}
