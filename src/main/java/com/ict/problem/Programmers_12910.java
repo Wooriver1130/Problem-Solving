9d97afae4c90770f5dcb594d9b1d84d6812bcc07
=aphel <dldnrkdqkqh@gmail.com>
2024-12-04 13:14:32 +0900

    프로그램스/Lv1/나누어 떨어지는 숫자배열

---
+++ src/main/java/com/ict/problem/Programmers_12910.java
@@ -0,0 +1,32 @@
+package com.ict.problem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Programmers_12910 {
+// class Solution {
+//     public int[] solution(int[] arr, int divisor) {
+//         List<Integer> list = new ArrayList<Integer>();
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] % divisor == 0) {
+//                 list.add(i);
+//             }
+//         }
+//         int[] arr2;
+//         if (list.size() > 0) {
+//             arr2 = new int[list.size()];
+//             arr2[0] = -1;
+//             for (int j = 0; j < list.size(); j++) {
+//                 arr2[j] = arr[list.get(j)];
+//             }
+//             Arrays.sort(arr2);
+//         } else {
+//             arr2 = new int[1];
+//             arr2[0] = -1;
+//         }
+
+//         return arr2;
+//     }
+//}
+}
